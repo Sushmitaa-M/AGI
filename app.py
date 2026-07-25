29a93ec1aa149e1fe01e6ced285e9feabb1db188
Sushmitaa-M <sushmitaamahesh@gmail.com>
2026-07-25 13:53:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
+import os , re , urllib.parse , urllib.request
+from Flask import flask , jsonify , request , Request
 
+app = flask(__name__)
+req = urllib.request(f"https://www.youtube.com/search?" methods = (
